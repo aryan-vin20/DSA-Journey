5140f18fead39c827b3023bc32410911af685e98
Aryan <darsharyanofficial@gmail.com>
2026-05-18 20:21:41 +0530

    updated file

---
+++ lecture_02/conditional_statements.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    char ch;
+    cout<< "Enter character\n";
+    cin>> ch;
+    if(ch>='A' && ch<='Z'){ //mistake : Mai single quotes lagan bhul gya tha
+        cout<<"Upper case";
+    }else{
+        cout<<"Lower case";
+    }
+    return 0;
+}
+
+
+//if 
+//if_else 
+//if_else if_else if ... 
+//if_else if_else if_...._else
+
+
+
+
