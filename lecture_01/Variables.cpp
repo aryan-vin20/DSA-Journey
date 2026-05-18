facee1a3b9bb77111aae1dff698584f6dad0b70d
Aryan <darsharyanofficial@gmail.com>
2026-05-18 22:46:41 +0530

    Updated variables lecture

--- lecture_01/Variables.cpp
+++ lecture_01/Variables.cpp
@@ -14,8 +14,8 @@ int main(){
 //cpp is a case sensitive language
 //always end the line with ;
 // -select the lines to comment and press ctrl /
-// for single char we use 'a'
-// for multiple char we use "abcd"
+// for single char we use 'a'  i.e single quotes ''
+// for multiple char we use "abcd"  i.e double quotes ""
 // variable names must start with either _ or alphabet(cannot start with digits)
 // 8 bits = 1 bytes
 
